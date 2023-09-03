1ea84453e2d28d3df8ddd0671ee045360caecdea
Theo Harris <theoharris@Theos-MacBook-Pro.local>
2023-09-03 16:25:24 +1000

    feat(index): Add link to blog to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
-
+import Link from "next/link";
+import { FiArrowRight } from "react-icons/fi";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between  lg:flex">
-        Hello world!
+        <Link
+          href="/blog"
+          className="flex flex-row space-x-4 items-center text-2xl hover:underline"
+        >
+          Blog Posts<FiArrowRight className="ml-2"></FiArrowRight>
+        </Link>
       </div>
     </main>
   );
