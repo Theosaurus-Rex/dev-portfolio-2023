4b34d33e316f7ae4f882744bd7b63b57542fd3c0
Theo Harris <theosaurus13@gmail.com>
2023-09-17 15:25:11 +1000

    feature(talks): Add TalkMetadata type and module for retrieving and passing TalkMetadata from markdown files

---
+++ src/app/talks/talks/page.tsx
@@ -0,0 +1,12 @@
+const TalksIndex = () => {
+  const talkMetadata = getTalkMetadata();
+  const talkPreviews = talkMetadata.map((talk) => (
+    <TalkPreview key={talk.slug} {...talk} />
+  ));
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">{talkPreviews}</div>
+  );
+};
+
+export default TalksIndex;
