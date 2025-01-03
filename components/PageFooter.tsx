35284eb4c26cd0f619cbcee9fc53afb1f4deb487
Theosaurus-Rex <theosaurus13@gmail.com>
2025-01-03 16:35:24 +1100

    fix(a11y): Add labels to icons in footer

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -6,16 +6,19 @@ const PageFooter = () => {
       <div className="border-t border-gray-500 mt-6 py-6 text-center text-gray-600 flex flex-col space-y-4 items-center">
         <div className="flex space-x-4">
           <SocialIcon
+            label="See Theo on LinkedIn"
             url="https://www.linkedin.com/in/theo-harris-coder/"
             bgColor="#9333ea"
             target="_blank"
           ></SocialIcon>
           <SocialIcon
+            label="See Theo's profile on GitHub"
             url="https://github.com/Theosaurus-Rex"
             bgColor="#9333ea"
             target="_blank"
           ></SocialIcon>
           <SocialIcon
+            label="See Theo's design work on Dribbble"
             url="https://dribbble.com/Theosaurus-Rex"
             bgColor="#9333ea"
             target="_blank"
