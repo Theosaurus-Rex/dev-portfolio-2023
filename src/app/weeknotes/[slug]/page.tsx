0224385ca1296d4b56055808444392720b890097
Theosaurus-Rex <theosaurus13@gmail.com>
2025-03-13 13:12:38 +1100

    fix: Address warning about using params synchronously

--- src/app/weeknotes/[slug]/page.tsx
+++ src/app/weeknotes/[slug]/page.tsx
@@ -3,26 +3,10 @@ import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
 import getWeekNoteMetadata from "../../../../components/getWeekNoteMetadata";
 
-const formatDateRange = (start: string, end: string) => {
-  const startDate = new Date(start);
-  const endDate = new Date(end);
-
-  const options: Intl.DateTimeFormatOptions = {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  };
-
-  return `${startDate.toLocaleDateString(
-    "en-GB",
-    options
-  )} - ${endDate.toLocaleDateString("en-GB", options)}`;
-};
-
-const getWeekNoteContent = (slug: string) => {
+const getWeekNoteContent = async (slug: string) => {
   const folder = "weeknotes/";
   const file = `${folder}${slug}.md`;
-  const content = fs.readFileSync(file, "utf8");
+  const content = await fs.promises.readFile(file, "utf8");
   const matterResult = matter(content);
   return matterResult;
 };
@@ -34,21 +18,14 @@ export const generateStaticParams = async () => {
   }));
 };
 
-const WeekNotePage = (props: any) => {
-  const slug = props.params.slug;
-  const weeknote = getWeekNoteContent(slug);
-  const dateRange = formatDateRange(
-    weeknote.data.weekStart,
-    weeknote.data.weekEnd
-  );
+const WeekNotePage = async (props: any) => {
+  const params = await props.params;
+  const slug = params.slug;
+  const weeknote = await getWeekNoteContent(slug);
 
   return (
     <div>
-      <div className="my-12 text-center">
-        <h1 className="text-4xl text-gray-900 font-bold">{dateRange}</h1>
-      </div>
-
-      <article className="prose lg:prose-xl">
+      <article className="prose lg:prose-xl pt-12">
         <Markdown>{weeknote.content}</Markdown>
       </article>
     </div>
