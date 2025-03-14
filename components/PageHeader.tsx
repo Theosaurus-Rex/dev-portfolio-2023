e1025157c8d78ad5333d665b37fcd01cb27001bc
Theosaurus-Rex <theosaurus13@gmail.com>
2025-03-15 10:20:52 +1100

    style: Redo styles for PageHeader component

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -19,14 +19,11 @@ export default function Example() {
   };
 
   return (
-    <header className="bg-white">
-      <nav
-        className="flex max-w-7xl items-center justify-between p-6 lg:px-8"
-        aria-label="Global"
-      >
+    <header className="bg-blue font-kanit font-bold text-lg uppercase italic tracking-wider border-b-4 border-black p-2">
+      <nav className="flex items-center justify-between" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
-            <span className="px-4 py-2 rounded-md font-semibold text-lg hover:text-purple-600 hover:bg-purple-50">
+            <span className="px-4 py-2 rounded-md  hover:text-blue hover:bg-black">
               Theo Harris
             </span>
           </Link>
@@ -34,11 +31,14 @@ export default function Example() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-3 text-gray-700"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <Bars3Icon
+              className="h-6 w-6 text-black cursor-pointer hover:text-blue hover:bg-black rounded-md"
+              aria-hidden="true"
+            />
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
@@ -46,7 +46,7 @@ export default function Example() {
             <Link
               key={item.name}
               href={item.href}
-              className="px-4 py-2 rounded-md text-lg font-semibold leading-6 text-gray-900 hover:text-purple-600 hover:bg-purple-50"
+              className="px-4 py-2 rounded-md leading-6 hover:text-blue hover:bg-black"
             >
               {item.name}
             </Link>
@@ -60,16 +60,16 @@ export default function Example() {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-blue px-6 py-6 sm:max-w-sm sm:ring-4 sm:ring-black font-kanit font-bold text-lg uppercase italic tracking-wider">
           <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.5">
-              <span className="font-bold text-lg  hover:text-purple-600 hover:bg-purple-50">
+            <Link href="/" className="-m-4 p-1.5">
+              <span className="font-bold hover:text-blue hover:bg-black px-3 py-2 rounded-lg">
                 Theo Harris
               </span>
             </Link>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 rounded-md p-2.5 text-black hover:text-blue hover:bg-black cursor-pointer"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
@@ -77,14 +77,14 @@ export default function Example() {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="-my-6 divide-y-4 divide-black ">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
                   <Link
                     key={item.name}
                     href={item.href}
                     onClick={handleLinkClick}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:text-purple-600  hover:bg-purple-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:text-blue hover:bg-black"
                   >
                     {item.name}
                   </Link>
@@ -93,18 +93,24 @@ export default function Example() {
               <div className="flex flex-row justify-between pt-4 mx-2">
                 <SocialIcon
                   url="https://www.linkedin.com/in/theo-harris-coder/"
-                  bgColor="#9333ea"
+                  bgColor="white"
+                  fgColor="black"
                   target="_blank"
+                  style={{ border: "3px solid black", borderRadius: "50%" }}
                 ></SocialIcon>
                 <SocialIcon
                   url="https://github.com/Theosaurus-Rex"
-                  bgColor="#9333ea"
+                  bgColor="white"
+                  fgColor="black"
                   target="_blank"
+                  style={{ border: "3px solid black", borderRadius: "50%" }}
                 ></SocialIcon>
                 <SocialIcon
                   url="https://dribbble.com/Theosaurus-Rex"
-                  bgColor="#9333ea"
+                  bgColor="white"
+                  fgColor="black"
                   target="_blank"
+                  style={{ border: "3px solid black", borderRadius: "50%" }}
                 ></SocialIcon>
               </div>
             </div>
