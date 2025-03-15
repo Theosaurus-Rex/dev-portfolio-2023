155c84782f1f06e0b1056203fff9a63a3b5c88f3
Theosaurus-Rex <theosaurus13@gmail.com>
2025-03-15 14:24:01 +1100

    style: Rework styles for footer component

--- components/PageFooter.tsx
+++ components/PageFooter.tsx
@@ -3,30 +3,36 @@ import { SocialIcon } from "react-social-icons";
 const PageFooter = () => {
   return (
     <footer>
-      <div className="border-t border-gray-500 mt-6 py-6 text-center text-gray-600 flex flex-col space-y-4 items-center">
-        <div className="flex space-x-4">
+      <div className="border-t-4 bg-blue border-black py-6 text-center text-black flex flex-col-reverse md:flex-row items-center md:justify-between font-kanit font-bold text-lg uppercase italic tracking-wider px-4">
+        <p aria-label="Developed with love by Theo Harris">
+          Developed with ♡ by Theo Harris
+        </p>
+        <div className="flex space-x-4 mb-4 md:mb-0">
           <SocialIcon
             label="See Theo on LinkedIn"
             url="https://www.linkedin.com/in/theo-harris-coder/"
-            bgColor="oklch(0.627 0.265 303.9)"
+            bgColor="white"
+            fgColor="black"
             target="_blank"
+            style={{ border: "3px solid black", borderRadius: "50%" }}
           ></SocialIcon>
           <SocialIcon
             label="See Theo's profile on GitHub"
             url="https://github.com/Theosaurus-Rex"
-            bgColor="oklch(0.627 0.265 303.9)"
+            bgColor="white"
+            fgColor="black"
             target="_blank"
+            style={{ border: "3px solid black", borderRadius: "50%" }}
           ></SocialIcon>
           <SocialIcon
             label="See Theo's design work on Dribbble"
             url="https://dribbble.com/Theosaurus-Rex"
-            bgColor="oklch(0.627 0.265 303.9)"
+            bgColor="white"
+            fgColor="black"
             target="_blank"
+            style={{ border: "3px solid black", borderRadius: "50%" }}
           ></SocialIcon>
         </div>
-        <p aria-label="Developed with love by Theo Harris">
-          Developed with 💜 by Theo Harris
-        </p>
       </div>
     </footer>
   );
