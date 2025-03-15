2965ed523e83dda2f93b25f167595f2a2f491ace
Theosaurus-Rex <theosaurus13@gmail.com>
2025-03-15 15:06:47 +1100

    feat: Start adding the homepage hero intro text

---
+++ src/app/fonts.js
@@ -0,0 +1,7 @@
+import { Kanit } from 'next/font/google'
+
+export const kanit = Kanit({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  display: 'swap',
+})
