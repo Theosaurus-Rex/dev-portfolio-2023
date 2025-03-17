ecaac29e283bcfc0c70613c514a2a1492f8d126a
Theosaurus-Rex <theosaurus13@gmail.com>
2025-03-18 08:19:53 +1100

    feat: Style intro paragraphs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex flex-col  pt-4 bg-cream min-h-screen">
-      <section className="flex-1">
+    <main className="flex flex-col pt-4 bg-cream min-h-screen lg:px-24">
+      <section>
         <h1 className="sr-only">
           'Sup, I'm Theo, a web developer based out of Sydney, NSW.
         </h1>
-        <Image
-          src="/images/Header.png"
-          alt="'Sup, I'm Theo, a web developer based out of Sydney, NSW."
-          width="946"
-          height="819"
-          className="lg:hidden"
-        />
-        <div className="hidden lg:block mx-24">
+        <div className="lg:hidden">
+          <Image
+            src="/images/Header.png"
+            alt="'Sup, I'm Theo, a web developer based out of Sydney, NSW."
+            width="946"
+            height="819"
+          />
+        </div>
+        <div className="hidden lg:block">
           <Image
             src="/images/HeaderDesktop.png"
             alt="'Sup, I'm Theo, and I work as a web developer based in Sydney"
@@ -22,15 +23,15 @@ export default function Home() {
           />
         </div>
       </section>
-      <section className="flex-1 flex flex-col items-center justify-between space-y-4 p-4">
-        <div className="bg-blue p-4 border-4 border-black rounded-md flex-1">
-          <p className="font-kanit font-bold md:text-xl">
+      <section className="flex flex-col lg:flex-row items-center justify-between space-y-4 lg:space-x-4 lg:space-y-0 p-4 lg:items-stretch">
+        <div className="bg-blue p-4 border-4 border-black rounded-md lg:flex-1 lg:p-4">
+          <p className="font-kanit font-bold lg:font-semibold md:text-xl lg:text-2xl xl:text-4xl">
             I've been a full stack developer and technical consultant for 3+
             years, and I have a passion for the web as a storytelling medium.
           </p>
         </div>
-        <div className="bg-pink p-4 border-4 border-black rounded-md flex-1">
-          <p className="font-kanit font-bold md:text-xl">
+        <div className="bg-pink p-4 border-4 border-black rounded-md lg:flex-1 lg:p-4">
+          <p className="font-kanit font-bold lg:font-semibold md:text-xl lg:text-2xl xl:text-4xl">
             I care deeply about equal access to the web for everyone, and work
             hard to create digital experiences that are both accessible and
             enjoyable.
