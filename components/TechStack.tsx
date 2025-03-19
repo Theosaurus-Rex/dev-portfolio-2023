d3c1b9f27f22d0499efb3faf38343fa402632f19
Theosaurus-Rex <theosaurus13@gmail.com>
2025-03-20 10:49:30 +1100

    style: Adjust tile numbers and sizes in tech stack depending on breakpoint

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -16,6 +16,17 @@ const technologies = [
   { name: "Tailwind CSS", icon: "/images/tech_icons/tailwind.svg" },
   { name: "Elixir", icon: "/images/tech_icons/elixir.svg" },
   { name: "Phoenix", icon: "/images/tech_icons/phoenix.svg" },
+  // Repeating to fill enough space on larger breakpoints
+  { name: "HTML", icon: "/images/tech_icons/html.svg" },
+  { name: "CSS", icon: "/images/tech_icons/css.svg" },
+  { name: "JavaScript", icon: "/images/tech_icons/javascript.svg" },
+  { name: "TypeScript", icon: "/images/tech_icons/typescript.svg" },
+  { name: "PostgreSQL", icon: "/images/tech_icons/postgresql.svg" },
+  { name: "React", icon: "/images/tech_icons/react.svg" },
+  { name: "Next.js", icon: "/images/tech_icons/nextjs.svg" },
+  { name: "Tailwind CSS", icon: "/images/tech_icons/tailwind.svg" },
+  { name: "Elixir", icon: "/images/tech_icons/elixir.svg" },
+  { name: "Phoenix", icon: "/images/tech_icons/phoenix.svg" },
 ];
 
 const bgColors = ["bg-blue", "bg-purple", "bg-orange", "bg-pink", "bg-lime"];
@@ -25,8 +36,18 @@ const TechStack = () => {
     <div className="w-full tech-stack-fade">
       <Swiper
         modules={[Autoplay]}
-        spaceBetween={32}
-        slidesPerView={5}
+        slidesPerView={6}
+        spaceBetween={10}
+        breakpoints={{
+          // md breakpoint (768px)
+          768: {
+            slidesPerView: 7,
+          },
+          // lg breakpoint (1024px)
+          1024: {
+            slidesPerView: 10,
+          },
+        }}
         loop={true}
         speed={3000}
         autoplay={{
@@ -44,9 +65,11 @@ const TechStack = () => {
               alt={`${tech.name} icon`}
               width={60}
               height={60}
-              className={`${
-                bgColors[index % bgColors.length]
-              } border-3 border-black rounded-md p-2`}
+              className={`
+                w-16 h-16 md:w-24 md:h-24 lg:w-24 lg:h-24
+                ${bgColors[index % bgColors.length]}
+                border-3 border-black rounded-md p-2
+              `}
             />
           </SwiperSlide>
         ))}
