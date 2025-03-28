526f741c442b1c19d6dca92e3503c07577c72085
Theosaurus-Rex <theosaurus13@gmail.com>
2025-03-28 13:29:25 +1100

    style: Update branding for blog posts index

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,21 +1,30 @@
 import Link from "next/link";
 import { PostMetadata } from "./PostMetadata";
 
-const PostPreview = (props: PostMetadata) => {
+const bgColors = ["bg-blue", "bg-purple", "bg-pink", "bg-orange", "bg-lime"];
+
+interface PostPreviewProps extends PostMetadata {
+  index: number;
+}
+
+const PostPreview = (props: PostPreviewProps) => {
+  const bgColor = bgColors[props.index % bgColors.length];
+
   return (
-    <a href={`/blog/posts/${props.slug}`}>
-      <article key={props.slug} className="flex flex-col items-start ">
-        <div className="relative w-full bg-purple-400 rounded-2xl overflow-hidden group">
-          <img
-            src={props.imageUrl || ""}
-            alt=""
-            className="aspect-16/9 w-full object-cover grayscale transition-all duration-500 group-hover:grayscale-0 sm:aspect-2/1 lg:aspect-3/2"
-          />
-          <div className="absolute inset-0 bg-purple-500 opacity-30 transition-opacity duration-500 group-hover:opacity-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-        </div>
+    <Link href={`/blog/posts/${props.slug}`} className="h-full block">
+      <article
+        key={props.slug}
+        className={`flex flex-col items-start ${bgColor} p-6 border-4 border-black rounded-md h-full`}
+      >
+        <img
+          src={props.imageUrl || ""}
+          alt=""
+          className="aspect-16/9 w-full object-cover grayscale sm:aspect-2/1 lg:aspect-3/2 rounded-md border-4 border-black"
+        />
+
         <div className="max-w-xl">
           <div className="mt-8 flex items-center gap-x-4 text-sm">
-            <time className="text-gray-500">
+            <time className="uppercase font-bold italic">
               {new Date(props.date).toLocaleDateString("en-US", {
                 year: "numeric",
                 month: "long",
@@ -24,17 +33,17 @@ const PostPreview = (props: PostMetadata) => {
             </time>
           </div>
           <div className="group relative">
-            <h2 className="mt-3 text-xl font-semibold leading-6 text-gray-900 group-hover:text-purple-600">
+            <h2 className="mt-3 text-xl font-semibold leading-6 group-hover:underline">
               <span className="absolute inset-0" />
               {props.title}
             </h2>
-            <p className="mt-5 line-clamp-3 text-md leading-6 text-gray-600">
+            <p className="mt-2 line-clamp-3 text-md leading-6">
               {props.description}
             </p>
           </div>
         </div>
       </article>
-    </a>
+    </Link>
   );
 };
 
